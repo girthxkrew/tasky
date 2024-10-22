bf95e278c7717a98055c84da16d129e2b40c6a3e
girthxkrew <ryankyler1@hotmail.com>
2024-10-22 17:44:31 -0400

    Changed AuthenticationManager to handle APIError instead of AuthError

--- app/src/main/java/com/rkm/tasky/network/authentication/abstraction/AuthenticationManager.kt
+++ app/src/main/java/com/rkm/tasky/network/authentication/abstraction/AuthenticationManager.kt
@@ -2,22 +2,11 @@ package com.rkm.tasky.network.authentication.abstraction
 
 import com.rkm.tasky.network.util.NetworkError
 import com.rkm.tasky.util.result.EmptyResult
-import com.rkm.tasky.util.result.Error
 
 interface AuthenticationManager {
-    suspend fun logIn(email: String, password: String): EmptyResult<AuthError>
-    suspend fun logOut(): EmptyResult<AuthError>
-    suspend fun checkAuthentication(): EmptyResult<AuthError>
-    suspend fun getNewAccessToken(): EmptyResult<AuthError>
-    suspend fun registerUser(fullName: String, email: String, password: String): EmptyResult<AuthError>
-}
-
-enum class AuthError: Error {
-    NO_INTERNET,
-    LOGIN_FAILED,
-    LOGOUT_FAILED,
-    AUTHENTICATION_CHECK_FAILED,
-    NO_USER_SESSION_IN_DATASTORE,
-    GET_NEW_ACCESS_TOKEN_FAILED,
-    REGISTRATION_FAILED
+    suspend fun logIn(email: String, password: String): EmptyResult<NetworkError.APIError>
+    suspend fun logOut(): EmptyResult<NetworkError.APIError>
+    suspend fun checkAuthentication(): EmptyResult<NetworkError.APIError>
+    suspend fun getNewAccessToken(): EmptyResult<NetworkError.APIError>
+    suspend fun registerUser(fullName: String, email: String, password: String): EmptyResult<NetworkError.APIError>
 }
--- app/src/main/java/com/rkm/tasky/network/authentication/implementation/AuthenticationManagerImpl.kt
+++ app/src/main/java/com/rkm/tasky/network/authentication/implementation/AuthenticationManagerImpl.kt
@@ -1,16 +1,13 @@
 package com.rkm.tasky.network.authentication.implementation
 
 import com.rkm.tasky.di.IoDispatcher
-import com.rkm.tasky.network.authentication.abstraction.AuthError
 import com.rkm.tasky.network.authentication.abstraction.AuthenticationManager
 import com.rkm.tasky.network.model.dto.asAuthInfo
 import com.rkm.tasky.network.repository.abstraction.AuthenticationRepository
 import com.rkm.tasky.network.util.NetworkError
 import com.rkm.tasky.util.result.EmptyResult
-import com.rkm.tasky.util.result.Error
 import com.rkm.tasky.util.result.Result
 import com.rkm.tasky.util.result.asEmptyDataResult
-import com.rkm.tasky.util.result.onFailure
 import com.rkm.tasky.util.result.onSuccess
 import com.rkm.tasky.util.storage.abstraction.SessionStorage
 import kotlinx.coroutines.CoroutineDispatcher
@@ -25,76 +22,60 @@ class AuthenticationManagerImpl @Inject constructor(
     override suspend fun logIn(
         email: String,
         password: String
-    ): EmptyResult<AuthError> =
+    ): EmptyResult<NetworkError.APIError> =
         withContext(dispatcher) {
             val result = repository.loginUser(email = email, password = password)
             result.onSuccess { user ->
                 sessionStorage.setSession(user.asAuthInfo())
             }
-            return@withContext handleResults(result, AuthError.LOGIN_FAILED)
+            return@withContext result.asEmptyDataResult()
         }
 
-    override suspend fun logOut(): EmptyResult<AuthError> = withContext(dispatcher) {
+    override suspend fun logOut(): EmptyResult<NetworkError.APIError> = withContext(dispatcher) {
         val result = repository.logoutUser()
         result.onSuccess {
             sessionStorage.clearSession()
         }
-        return@withContext handleResults(result, AuthError.LOGOUT_FAILED)
+        return@withContext result.asEmptyDataResult()
     }
 
-    override suspend fun checkAuthentication(): EmptyResult<AuthError> =
+    override suspend fun checkAuthentication(): EmptyResult<NetworkError.APIError> =
         withContext(dispatcher) {
-            return@withContext handleResults(
-                repository.checkAuthentication(),
-                AuthError.AUTHENTICATION_CHECK_FAILED
-            )
+            return@withContext checkAuthentication().asEmptyDataResult()
         }
 
-    override suspend fun getNewAccessToken(): EmptyResult<AuthError> = withContext(dispatcher) {
-        val authInfo = sessionStorage.getSession() ?: return@withContext Result.Error(
-            AuthError.NO_USER_SESSION_IN_DATASTORE
-        )
+    override suspend fun getNewAccessToken(): EmptyResult<NetworkError.APIError> =
+        withContext(dispatcher) {
+            val authInfo = sessionStorage.getSession() ?: return@withContext Result.Error(
+                NetworkError.APIError.UNKNOWN
+            )
 
-        val result = repository.getNewAccessToken(
-            refreshToken = authInfo.refreshToken,
-            userId = authInfo.userId
-        )
+            val result = repository.getNewAccessToken(
+                refreshToken = authInfo.refreshToken,
+                userId = authInfo.userId
+            )
 
-        result.onSuccess { newAccessToken ->
-            sessionStorage.setSession(
-                authInfo.copy(
-                    accessToken = newAccessToken.accessToken,
-                    accessTokenExpirationTimestamp = newAccessToken.expirationTimestamp
+            result.onSuccess { newAccessToken ->
+                sessionStorage.setSession(
+                    authInfo.copy(
+                        accessToken = newAccessToken.accessToken,
+                        accessTokenExpirationTimestamp = newAccessToken.expirationTimestamp
+                    )
                 )
-            )
-        }
+            }
 
-        return@withContext handleResults(result, AuthError.GET_NEW_ACCESS_TOKEN_FAILED)
-    }
+            return@withContext result.asEmptyDataResult()
+        }
 
     override suspend fun registerUser(
         fullName: String,
         email: String,
         password: String
-    ): EmptyResult<AuthError> = withContext(dispatcher) {
-        val result =
-            repository.registerUser(fullName = fullName, email = email, password = password)
-        return@withContext handleResults(result, AuthError.REGISTRATION_FAILED)
-    }
-
-    private fun handleResults(
-        result: Result<Any, Error>,
-        error: AuthError
-    ): EmptyResult<AuthError> {
-        return when (result) {
-            is Result.Success -> result.asEmptyDataResult()
-            is Result.Error -> {
-                if (result.error == NetworkError.APIError.NO_INTERNET) {
-                    Result.Error(AuthError.NO_INTERNET)
-                } else {
-                    Result.Error(error)
-                }
-            }
-        }
+    ): EmptyResult<NetworkError.APIError> = withContext(dispatcher) {
+        return@withContext repository.registerUser(
+            fullName = fullName,
+            email = email,
+            password = password
+        ).asEmptyDataResult()
     }
 }
