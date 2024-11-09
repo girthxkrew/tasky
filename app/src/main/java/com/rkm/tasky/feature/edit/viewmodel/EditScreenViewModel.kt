e537103866ce64290ed5a3578767128ad79d9d2b
girthxkrew <ryankyler1@hotmail.com>
2024-11-08 20:16:04 -0500

    Adding Edit Screen

---
+++ app/src/main/java/com/rkm/tasky/feature/edit/viewmodel/EditScreenViewModel.kt
@@ -0,0 +1,31 @@
+package com.rkm.tasky.feature.edit.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
+import com.rkm.tasky.navigation.Edit
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class EditScreenViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+    private companion object {
+        const val TEXT_KEY = "TEXT"
+    }
+    private val edit = savedStateHandle.toRoute<Edit>()
+
+    val action = edit.action
+
+    private val _text = MutableStateFlow(savedStateHandle[TEXT_KEY] ?: edit.text)
+    val text = _text.asStateFlow()
+
+    fun updateText(text: String) {
+        _text.update { text }
+        savedStateHandle[TEXT_KEY] = text
+    }
+}
--- app/src/main/java/com/rkm/tasky/navigation/AppNavigation.kt
+++ app/src/main/java/com/rkm/tasky/navigation/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.rkm.tasky.navigation
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -8,30 +9,17 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import com.rkm.tasky.R
 import com.rkm.tasky.feature.agenda.screen.AgendaScreenRoot
 import com.rkm.tasky.feature.agenda.viewmodel.AgendaViewModel
+import com.rkm.tasky.feature.edit.screen.EditScreenRoot
+import com.rkm.tasky.feature.edit.viewmodel.EditScreenViewModel
 import com.rkm.tasky.feature.login.screen.LoginScreenRoot
 import com.rkm.tasky.feature.login.viewmodel.LoginViewModel
 import com.rkm.tasky.feature.registration.screen.RegistrationScreenRoot
 import com.rkm.tasky.feature.registration.viewmodel.RegistrationViewModel
 import com.rkm.tasky.ui.activity.AuthState
 import kotlinx.serialization.Serializable
-
-@Serializable
-object Authentication
-
-@Serializable
-object Login
-
-@Serializable
-object Registration
-
-@Serializable
-object Home
-
-@Serializable
-object Agenda
-
 
 @Composable
 fun AppNavigation(
@@ -42,26 +30,30 @@ fun AppNavigation(
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = if (authState == AuthState.AuthenticatedUser) Home else Authentication
+        startDestination = if (authState == AuthState.AuthenticatedUser) Destination.Home else Destination.Authentication
     ) {
 
-        navigation<Home>(startDestination = Agenda) {
-            composable<Agenda> {
+        navigation<Destination.Home>(startDestination = Destination.Agenda) {
+            composable<Destination.Agenda> {
                 AgendaScreenRoot(
                     modifier = modifier,
                     viewModel = hiltViewModel<AgendaViewModel>()
                 )
             }
+
+            composable<Destination.Edit> {
+                TODO("Add Logic to handle navigation")
+            }
         }
 
-        navigation<Authentication>(startDestination = Login) {
-            composable<Login> {
+        navigation<Destination.Authentication>(startDestination = Destination.Login) {
+            composable<Destination.Login> {
                 LoginScreenRoot(
-                    onRegistrationClick = { navController.navigate(Registration) },
+                    onRegistrationClick = { navController.navigate(Destination.Registration) },
                     onLoginSuccess =
                     {
-                        navController.navigate(Agenda) {
-                            popUpTo(Authentication) {
+                        navController.navigate(Destination.Agenda) {
+                            popUpTo(Destination.Authentication) {
                                 inclusive = true
                             }
                         }
@@ -71,11 +63,11 @@ fun AppNavigation(
                 )
             }
 
-            composable<Registration> {
+            composable<Destination.Registration> {
                 RegistrationScreenRoot(
                     onNavigateBack = {
-                        navController.navigate(Login) {
-                            popUpTo(Login) {
+                        navController.navigate(Destination.Login) {
+                            popUpTo(Destination.Login) {
                                 inclusive = true
                             }
                         }
@@ -86,4 +78,30 @@ fun AppNavigation(
             }
         }
     }
+}
+
+sealed class Destination {
+    @Serializable
+    data object Authentication : Destination()
+
+    @Serializable
+    object Login : Destination()
+
+    @Serializable
+    object Registration : Destination()
+
+    @Serializable
+    object Home : Destination()
+
+    @Serializable
+    object Agenda : Destination()
+
+    @Serializable
+    data class Edit(val text: String, val action: EditActionType) : Destination()
+}
+
+@Serializable
+sealed class EditActionType(@StringRes val id: Int) {
+    data object Title : EditActionType(R.string.edit_screen_toolbar_title)
+    data object Description : EditActionType(R.string.edit_screen_toolbar_description)
 }
